bbf7008b97732e7c7d4bf3ad8389b28cee9ba27f
Javier Rodriguez <jjrodriguezc@uce.edu.ec>
2025-07-19 23:15:02 -0500

    add mongo

---
+++ mongo/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { connectToMongo } = require('./src/config/mongo');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// Rutas...
+const logRoutes = require('./src/routes/log.routes');
+app.use('/api/logs', logRoutes);
+app.use('/api/ventas', require('./routes/ventas.routes'));
+
+
+async function startServer() {
+  await connectToMongo();
+  app.listen(PORT, () => {
+    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+  });
+}
+
+startServer();
